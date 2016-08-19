a2cb8dbdcc807613c55e3d134af0c1bfce354f01
jszod <joe.szod@gmail.com>
2016-08-19 16:28:08 -0400

    Install emacs and markdown via laptop.local, add emacs.d dir to dotfiles

---
+++ emacs.d/SessionDesktop.el
@@ -0,0 +1,33 @@
+;; -*- mode: emacs-lisp; coding: emacs-mule; -*-
+;; --------------------------------------------------------------------------
+;; Desktop File for Emacs
+;; --------------------------------------------------------------------------
+;; Created Wed Aug 17 18:26:29 2016
+;; Desktop file format version 206
+;; Emacs version 24.4.51.2
+(setq revive:frame-configuration-to-restore
+  '(progn 
+     (revive:restore-frame '((tool-bar-position . top) (explicit-name) (icon-name) (bottom-divider-width . 0) (right-divider-width . 0) (top . 23) (left . 0) (unsplittable) (modeline . t) (width . 201) (height . 49) (visibility . t) (cursor-color . "Red") (background-mode . light) (fringe) (fullscreen) (alpha) (scroll-bar-width . 15) (cursor-type . box) (auto-lower) (auto-raise) (icon-type) (title) (buffer-predicate) (tool-bar-lines . 1) (menu-bar-lines . 1) (right-fringe . 11) (left-fringe . 3) (line-spacing) (background-color . "White") (foreground-color . "Black") (vertical-scroll-bars . right) (internal-border-width . 0) (border-width . 0) (font . "-*-Monaco-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1") (fontsize . 0)) '(201 49 ((0 0 206 25) (0 25 103 49) (103 25 206 49)) ((nil "untitled" 1 1 nil) (nil "untitled" 1 1 nil) (nil "untitled" 1 1 nil))))))
+
+;; Global section:
+(setq desktop-saved-frameset [frameset 1 (22452 58517 625222 0) (desktop . "206") "jszodfridt@JSzod-MBP.home" nil nil ((((font-backend mac-ct ns) (fontsize . 0) (font . "-*-Monaco-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1") (border-width . 0) (internal-border-width . 0) (vertical-scroll-bars . right) (foreground-color . "Black") (background-color . "White") (line-spacing) (left-fringe . 3) (right-fringe . 11) (menu-bar-lines . 1) (tool-bar-lines . 1) (title) (icon-type) (auto-raise) (auto-lower) (cursor-type . box) (scroll-bar-width . 15) (alpha) (fullscreen) (fringe) (display-type . color) (background-mode . light) (cursor-color . "Red") (visibility . t) (frameset--id . "51AC-0638-5D97-CF40") (frameset--mini t) (height . 49) (width . 201) (modeline . t) (minibuffer . t) (unsplittable) (left . 0) (top . 23) (right-divider-width . 0) (bottom-divider-width . 0) (icon-name) (display . "JSzod-MBP.home") (explicit-name) (tool-bar-position . top)) ((min-height . 8) (min-width . 20) (min-height-ignore . 4) (min-width-ignore . 14) (min-height-safe . 2) (min-width-safe . 4) (min-pixel-height . 128) (min-pixel-width . 140) (min-pixel-height-ignore . 64) (min-pixel-width-ignore . 86) (min-pixel-height-safe . 32) (min-pixel-width-safe . 28)) vc (pixel-width . 1440) (pixel-height . 779) (total-width . 206) (total-height . 49) (normal-height . 1.0) (normal-width . 1.0) (combination-limit) (leaf (pixel-width . 1440) (pixel-height . 390) (total-width . 206) (total-height . 25) (normal-height . 0.5) (normal-width . 1.0) (buffer "untitled" (selected . t) (hscroll . 0) (fringes 3 11 nil) (margins nil) (scroll-bars 15 3 t nil) (vscroll . 0) (dedicated) (point . 1) (start . 1))) (hc (last . t) (pixel-width . 1440) (pixel-height . 389) (total-width . 206) (total-height . 24) (normal-height . 0.5) (normal-width . 1.0) (combination-limit) (leaf (pixel-width . 720) (pixel-height . 389) (total-width . 103) (total-height . 24) (normal-height . 1.0) (normal-width . 0.5) (buffer "untitled" (selected) (hscroll . 0) (fringes 3 11 nil) (margins nil) (scroll-bars 15 3 t nil) (vscroll . 0) (dedicated) (point . 1) (start . 1))) (leaf (last . t) (pixel-width . 720) (pixel-height . 389) (total-width . 103) (total-height . 24) (normal-height . 1.0) (normal-width . 0.5) (buffer "untitled" (selected) (hscroll . 0) (fringes 3 11 nil) (margins nil) (scroll-bars 15 3 t nil) (vscroll . 0) (dedicated) (point . 1) (start . 1))))) (((font-backend mac-ct ns) (fontsize . 0) (font . "-*-Monaco-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1") (border-width . 0) (internal-border-width . 0) (vertical-scroll-bars . right) (foreground-color . "Black") (background-color . "White") (line-spacing) (left-fringe . 3) (right-fringe . 11) (menu-bar-lines . 1) (tool-bar-lines . 1) (title) (icon-type) (auto-raise) (auto-lower) (cursor-type . box) (scroll-bar-width . 15) (alpha) (fullscreen) (fringe) (display-type . color) (background-mode . light) (cursor-color . "Red") (visibility) (frameset--id . "2F97-4858-1A90-CE62") (frameset--mini t . t) (height . 41) (width . 164) (modeline . t) (minibuffer . t) (unsplittable) (left . 170) (top . 112) (right-divider-width . 0) (bottom-divider-width . 0) (icon-name) (display . "JSzod-MBP.home") (explicit-name . t) (tool-bar-position . top)) ((min-height . 4) (min-width . 10) (min-height-ignore . 2) (min-width-ignore . 7) (min-height-safe . 1) (min-width-safe . 2) (min-pixel-height . 64) (min-pixel-width . 70) (min-pixel-height-ignore . 32) (min-pixel-width-ignore . 43) (min-pixel-height-safe . 16) (min-pixel-width-safe . 14)) leaf (pixel-width . 1177) (pixel-height . 640) (total-width . 168) (total-height . 40) (normal-height . 1.0) (normal-width . 1.0) (buffer " *empty*" (selected) (hscroll . 0) (fringes 3 11 nil) (margins nil) (scroll-bars 15 3 t nil) (vscroll . 0) (dedicated) (point . 1) (start . 1))))])
+(setq desktop-missing-file-warning nil)
+(setq tags-file-name nil)
+(setq tags-table-list nil)
+(setq search-ring nil)
+(setq regexp-search-ring nil)
+(setq register-alist nil)
+(setq file-name-history '("~/code/elixir/visualist/apps/storymapping/mix.exs" "~/code/elixir/visualist" "~/.emacs.d/init.el"))
+
+;; Buffer section -- buffers listed in same order as in buffer list:
+(desktop-create-buffer 206
+  "/Users/joeszodfridt/Library/Application Support/Aquamacs Emacs/scratch buffer"
+  "*scratch*"
+  'text-mode
+  '(auto-fill-mode smart-spacing-mode)
+  1
+  '(22 nil)
+  nil
+  nil
+  '((buffer-file-coding-system . utf-8) (truncate-lines)))
+
