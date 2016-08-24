65231605717cc23afed740f23b45fe4a3bccc80d
jszod <joe.szod@gmail.com>
2016-08-23 21:56:05 -0400

    Add Alchemist to emacs, default font to Inconsolat, Install Inconsolata font via brew cask

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,4 +1,3 @@
-
 ;; Turn off the welcome message
 (setq inhibit-startup-message t)
 
@@ -9,17 +8,30 @@
 ;; Start full height
 (add-to-list 'initial-frame-alist '(fullscreen . fullheight))
 
+
+;; Set default font to monaco
+;; (set-face-font 'default "-*-Monaco-normal-r-*-*-17-102-120-120-c-*-iso8859-1")
+;; (set-face-font 'default "-*-Monaco-normal-r-*-*-12-102-120-120-c-*-iso8859-1")
+(set-face-font 'default "Inconsolata-14")
+
 ;; Enable Package.el package manager
-(require 'package) ;; You might already have this line
-(add-to-list 'package-archives
-             '("melpa-stable" . "https://melpa.org/packages/"))
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("marmalade" . "https://marmalade-repo.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
-(package-initialize) ;; You might already have this line
+(package-initialize) ;; You might already have this
 
-; Add some local themes
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+;; Package specific setups
+;; exec-path-from-shell
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
+;; Add some local themes
+;;(load-theme 'tango-dark' t)
+(load-theme 'railscasts' t)
 
 
 (custom-set-variables
@@ -27,12 +39,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
  '(custom-safe-themes
    (quote
-    ("ad950f1b1bf65682e390f3547d479fd35d8c66cafa2b8aa28179d78122faa947" "557c283f4f9d461f897b8cac5329f1f39fac785aa684b78949ff329c33f947ec" "f9b66dab1b1dafe4ed3d6e5af52a26b5197570ce353e60b665509ee617de5a43" "06b2849748590f7f991bf0aaaea96611bb3a6982cad8b1e3fc707055b96d64ca" default))))
+    ("cdbd0a803de328a4986659d799659939d13ec01da1f482d838b68038c1bb35e8" "557c283f4f9d461f897b8cac5329f1f39fac785aa684b78949ff329c33f947ec" "12b4427ae6e0eef8b870b450e59e75122d5080016a9061c9696959e50d578057" "ad950f1b1bf65682e390f3547d479fd35d8c66cafa2b8aa28179d78122faa947" "4f5bb895d88b6fe6a983e63429f154b8d939b4a8c581956493783b2515e22d6d" "3b0a350918ee819dca209cec62d867678d7dac74f6195f5e3799aa206358a983" default)))
+ '(markdown-command "/usr/local/bin/markdown"))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
