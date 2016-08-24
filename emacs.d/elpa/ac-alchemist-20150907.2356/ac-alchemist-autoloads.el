3fb2bdc3f135881e276973a2799f11f7c87e22b5
jszod <joe.szod@gmail.com>
2016-08-24 11:06:08 -0400

    Ignoring emacs.d files that don't require version control

--- emacs.d/elpa/ac-alchemist-20150907.2356/ac-alchemist-autoloads.el
+++
@@ -1,27 +0,0 @@
-;;; ac-alchemist-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "ac-alchemist" "ac-alchemist.el" (22460 58784
-;;;;;;  0 0))
-;;; Generated autoloads from ac-alchemist.el
-
-(autoload 'ac-alchemist-setup "ac-alchemist" "\
-
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("ac-alchemist-pkg.el") (22460 58784 161960
-;;;;;;  0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; ac-alchemist-autoloads.el ends here
